33135e82121d10b8c040d19732fde61598279ec6
unknown <esonovjahongir22@gmail.com>
2024-10-09 14:23:46 +0500

    first commit

---
+++ oopJava1/src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+
+    // Atributlar
+    private String color;    // Mashina rangi
+    private String model;    // Mashina modeli
+    private double price;    // Mashina narxi
+    private boolean isAutomatic;  // Avtomat yoki mexanik tuzilma
+}
